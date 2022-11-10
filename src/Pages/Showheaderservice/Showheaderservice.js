8eeb3fb6a2ada10737038657272dcda1a12b6e00
ashr-aful007 <ashraful99980@gmail.com>
2022-11-10 23:46:17 +0600

    add photo view

--- src/Pages/Showheaderservice/Showheaderservice.js
+++ src/Pages/Showheaderservice/Showheaderservice.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import { PhotoProvider, PhotoView } from 'react-photo-view';
+import 'react-photo-view/dist/react-photo-view.css';
 
 function Showheaderservice({servic}) {
      const {description,img,name,price,_id,} = servic;
   return (
      <div className="card w-full bg-base-100 shadow-xl">
-     <figure><img className='w-full h-15' src={img} alt="Shoes" /></figure>
+          <PhotoProvider>
+      <PhotoView src={img}>
+      <figure><img className='w-full h-15' src={img} alt="Shoes" /></figure>
+      </PhotoView>
+    </PhotoProvider>    
      <div className="card-body">
        <h2 className="card-title">{name}</h2>
        <p>{description.slice(0,100)}</p>
